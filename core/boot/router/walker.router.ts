41e1315a943d7dcb26afa9d4b5525f5b83d99e8c
Nicolas-Asafe <nicolasasafe45@gmail.com>
2026-01-21 13:20:40 -0300

    fix: walker route render paths

--- core/boot/router/walker.router.ts
+++ core/boot/router/walker.router.ts
@@ -1,4 +1,5 @@
 import fs from "fs/promises"
+import path from "path"
 import { loadRoute } from "../loader/route/route.loader"
 import { toConsole } from "../../shared/loggers/logger"
 import { useRoute } from "./useRoute.router"
@@ -6,28 +7,59 @@ import { routeEntity } from "../loader/route/route.entity"
 import { userEntity } from "../distros_tools/entitys/user.entity"
 import { distroEntity } from "../loader/distros/distro.entity"
 
-export async function walkerRouter(app: any, renderDirPath:  string,distros: distroEntity[],user:userEntity) {
+export async function walkerRouter(
+    app: any,
+    renderDirPath: string,
+    distros: distroEntity[],
+    user: userEntity,
+    renderRootPath: string = renderDirPath
+) {
     const renderDir = await fs.readdir(renderDirPath, { withFileTypes: true })
-    const routes = []
+    const routes: routeEntity[] = []
     for (const dirent of renderDir) {
+        const currentPath = path.join(renderDirPath, dirent.name)
         if (dirent.isDirectory()) {
-            await walkerRouter(app, `${renderDirPath}/${dirent.name}`,distros,user)
-        } 
-        if (dirent.isFile() && dirent.name=="index.json") {
-            const routePath = `${renderDirPath}/${dirent.name}`
-            const routeApiPath = dirent.parentPath.split("/").reverse()
-            const loadRouteResponse = await loadRoute(routePath,routeApiPath[0],distros)
+            const childRoutes = await walkerRouter(
+                app,
+                currentPath,
+                distros,
+                user,
+                renderRootPath
+            )
+            routes.push(...childRoutes)
+            continue
+        }
+        if (dirent.isFile() && dirent.name === "index.json") {
+            const relativeDir = path.relative(
+                renderRootPath,
+                path.dirname(currentPath)
+            )
+            const routeApiPath = relativeDir.replace(/\\/g, "/")
+            const loadRouteResponse = await loadRoute(
+                currentPath,
+                routeApiPath,
+                distros
+            )
             if (!loadRouteResponse.ok) {
-                toConsole(loadRouteResponse,"TomatoLoadRoute",`Error loading route from ${routePath}. Err: ${loadRouteResponse.details.error.message}`)
-                continue;
+                toConsole(
+                    loadRouteResponse,
+                    "TomatoLoadRoute",
+                    `Error loading route from ${currentPath}. Err: ${loadRouteResponse.details.error.message}`
+                )
+                continue
             }
-            const route:routeEntity = loadRouteResponse.details.route
-            console.log(route)
-            useRoute(app,route,distros,user)
-            toConsole(loadRouteResponse,"TomatoWalkerRouter",`Route loaded from ${routePath}`)
+            const route: routeEntity = loadRouteResponse.details.route
+            useRoute(app, route, distros, user)
+            toConsole(
+                loadRouteResponse,
+                "TomatoWalkerRouter",
+                `Route loaded from ${currentPath}`
+            )
+
             routes.push(route)
         }
     }
+
     return routes
 }
 
