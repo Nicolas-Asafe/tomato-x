0de6aa5ab949c3cc22677b5dd44e81d2f9f85eb1
Nicolas-Asafe <nicolasasafe45@gmail.com>
2026-01-27 20:08:22 -0300

    fix: engine details version to 26.2.2.2

--- usrl/distros/tomato/app.distro.ts
+++ usrl/distros/tomato/app.distro.ts
@@ -2,7 +2,7 @@ import {TomatoEntitys,TomatoDecorators} from "tomato-contracts"
 import path from "path";
 @TomatoDecorators.DistroApplicationDecorator({
     __distro_name:"tomato",
-    __compatibility_version:"26.2.1.5"
+    __compatibility_version:"26.2.2.2"
     ,__version:"1.0.0"
 })
 export default class TomatoDistroApp {
