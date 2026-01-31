b45ce3d8e77c2b2d3270a8d57dc8c4c64343530b
Nicolas Asafe <nicolasasafe45@gmail.com>
2026-01-30 23:01:28 -0300

    otimize: base.loader.ts

--- tx/boot/loader/base/base.loader.ts
+++ tx/boot/loader/base/base.loader.ts
@@ -1,33 +1,25 @@
-import fs from "fs/promises"
-import { baseEntity } from "./base.entity";
+export async function loadBases(
+  location: string,
+  distroname: string
+): Promise<baseEntity[]> {
 
-export async function loadBases(location:string,distroname:string){
-    const dir = await fs.readdir(location, { withFileTypes: true })
-    const bases_path = [];
-    const bases = [];
-    for (const dirent of dir) {
-        if (dirent.isFile() && dirent.name.endsWith(".base.ts")) {
-            bases_path.push(dirent.name);
-        }
+  const dir = await fs.readdir(location, { withFileTypes: true })
+
+  const loaders = []
+
+  for (let i = 0; i < dir.length; i++) {
+    const d = dir[i]
+    if (d.isFile() && d.name.endsWith(".base.ts")) {
+      loaders.push(
+        import(`../../../../${location}${d.name}`).then(mod => {
+          const base: baseEntity = new mod.default()
+          base.distro = distroname
+          base.location = location
+          return base
+        })
+      )
     }
-    for (const base_path of bases_path) {
-        const baseModule = await import(`../../../../${location}${base_path}`);
-        if (!baseModule) throw new Error(`the base module ${base_path} not found`)
+  }
 
-        const baseClass = baseModule.default;
-        const baseInstance = new baseClass()
-
-        const base = {
-            __name:baseInstance.__name,
-            distro:distroname,
-            exec:baseInstance.exec,
-            parse:baseInstance.parse,
-            logic:baseInstance.logic,
-            location:location,
-            setCtx:baseInstance.setCtx,
-            keys:baseInstance.keys,
-        } as baseEntity
-        bases.push(base);
-    }
-    return bases;
+  return Promise.all(loaders)
 }
